c946cfa66cc6849c50263e47ada9b067d6eaa94f
Mister kevin <pascal.d.kevin@gmail.com>
2017-05-03 17:51:44 +0200

    insertion de la semaine2

---
+++ semaine_2/JS/tp.js
@@ -0,0 +1,46 @@
+var question[
+	{
+		ques:"Es-ce que tu aime le Javascript ?",
+		answer:"yes"
+	},
+
+	{
+		ques:"Le JavaScript est une évolution du Java?",
+		answer:"non"
+	},
+
+	{
+		ques:"Le JavaScript est proche du Java",
+		answer:"non"
+	},
+
+	{
+		ques:"Le Javascript est un language d'objet par prototypage?",
+		answer:"oui"
+	}
+
+	{
+		ques:"Le JavaScript est souvent utilisé coté client?",
+		answer:"Oui"
+	}
+
+	{
+		ques:"Le Javascript peut être utilisé coté client et serveur?",
+		answer:"oui"
+	}
+]
+
+for(i=0; question[i].ques<=question.lenght; i++){
+	var reponse= prompt("question[i].ques");
+	toLowerCase()===reponse;
+	if (reponse===(toLowerCase()question[i].answer)){
+		console.log("Bien jouer, la reponse à la question à la question question[")
+	}
+
+
+
+
+
+
+
+]
