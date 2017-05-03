bc9fdcdb51fedbc26dd802db770397267cd49616
kevin pascal <pascal.d.kevin@gmail.com>
2017-05-04 01:37:56 +0200

    ajout dossier CV et tp pour mardi

--- semaine_2/JS/tp.js
+++ semaine_2/JS/tp.js
@@ -1,7 +1,7 @@
-var question[
+var question=[
 	{
 		ques:"Es-ce que tu aime le Javascript ?",
-		answer:"yes"
+		answer:"oui"
 	},
 
 	{
@@ -17,30 +17,99 @@ var question[
 	{
 		ques:"Le Javascript est un language d'objet par prototypage?",
 		answer:"oui"
-	}
+	},
 
 	{
 		ques:"Le JavaScript est souvent utilisé coté client?",
-		answer:"Oui"
-	}
+		answer:"oui"
+	},
 
 	{
 		ques:"Le Javascript peut être utilisé coté client et serveur?",
 		answer:"oui"
 	}
 ]
+console.log("Essaye de repondre à ce questionnaire!!!");
+console.log(question[0].ques);
+console.log(question.length);
+var bonneReponse=0;
+var mauvaiseReponse=0;
+var i=0;
 
-for(i=0; question[i].ques<=question.lenght; i++){
-	var reponse= prompt("question[i].ques");
-	toLowerCase()===reponse;
-	if (reponse===(toLowerCase()question[i].answer)){
-		console.log("Bien jouer, la reponse à la question à la question question[")
+var forQuestionnaire= function() {
+	alert("Repond à ces " + question.length + " questions par un simple oui ou non")
+	for(i=0; i<=(question.length-1); i++){
+		console.log("question N°"+(i+1)+ " sur " + question.length + " ."  );
+		var saisie= prompt(question[i].ques);
+		var lowerReponse= saisie.toLowerCase();
+		var reponse=lowerReponse.trim();
+		if (reponse===question[i].answer){
+			console.log("Bien jouer, c'est evidant que l'on peut répondre que " + question[i].answer + " à la question question : " + question[i].ques + "!!" )
+			bonneReponse++
+		}
+		else{
+			var veriter=question[i].answer.toUpperCase();
+			console.log("Serieux " + reponse +" !!! Refléchi. La question était : " + question[i].ques + " C'est évidant que la reponse est " + veriter + " !!!")
+			mauvaiseReponse++
+		}
 	}
 
+	var ratio= (bonneReponse-mauvaiseReponse)/question.length;
+	console.log("Ton taux de réussite est de : " + (ratio*100) + "%");
+	if(ratio===1){
+		alert("FELICITATION c'est un sans fautes!!");
+	}
+
+	else if (ratio<1&&ratio>=0){
+		alert("Cool tu a bien écouté");
+	}
+
+	else if (ratio<0&&ratio>-1){
+		alert("Quand l'éléve est nul, le prof ne peux rien faire");
+	}
+
+	else{
+		alert("Nan mais atend tu étais en classe???")
+	}
+}	
 
 
 
+var whileQuestionnaire= function () {
+		alert("Repond à ces " + question.length + " questions par un simple oui ou non")
+		while(i<=question.length-1){
+		console.log("question N°"+(i+1)+ " sur " + question.length + " ."  );
+		var saisie= prompt(question[i].ques);
+		var lowerReponse= saisie.toLowerCase();
+		var reponse=lowerReponse.trim();
+		if (reponse===question[i].answer){
+			console.log("Bien jouer, c'est evidant que l'on peut répondre que " + question[i].answer + " à la question question : " + question[i].ques + "!!" )
+			bonneReponse++
+			i++
+		}
+		else{
+			var veriter=question[i].answer.toUpperCase();
+			console.log("Serieux " + reponse +" !!! Refléchi. La question était : " + question[i].ques + " C'est évidant que la reponse est " + veriter + " !!!")
+			mauvaiseReponse++
+			i++
+		}
+	}
 
+	var ratio= (bonneReponse-mauvaiseReponse)/question.length;
+	console.log("Ton taux de réussite est de : " + (ratio*100) + "%");
+	if(ratio===1){
+		alert("FELICITATION c'est un sans fautes!!");
+	}
 
+	else if (ratio<1&&ratio>=0){
+		alert("Cool tu a bien écouté");
+	}
 
-]
+	else if (ratio<0&&ratio>-1){
+		alert("Quand l'éléve est nul, le prof ne peux rien faire");
+	}
+
+	else{
+		alert("Nan mais atend tu étais en classe???")
+	}
+}
