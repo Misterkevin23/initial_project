cec150c51ea76bb3cec2b41aa1c09a943d3375b4
Mister kevin <pascal.d.kevin@gmail.com>
2017-05-11 19:09:31 +0200

    fin semaine 3

--- semaine_3/js/panier.js
+++ semaine_3/js/panier.js
@@ -7,19 +7,20 @@ function aPayerParArticle(){
 		console.log(quantite)
 		if ( (quantite.value != "" && !isNaN (quantite.value)) && ( price.value != "" && !isNaN(price.value)) ) {
 			var calc = price.value * quantite.value;
-			console.log("hello2")
+			console.log(facturation.querySelector(".totalAPayer"))
 			facturation.querySelector(".totalAPayer").value = calc;
 		}	
 	})
 }
 
-function totalAPayer(){
+function TtotalAPayer(){
 	var total= document.querySelectorAll(".totalAPayer");
-	console.log("yes");
+	console.log(total);
 	total.forEach(function(toto){
 		var TtotalHT= toto.querySelector(".totalAPayer");
+		console.log(toto.querySelector(".totalAPayer"));
 		if( TtotalHT.value != "" && !isNaN(total.value)){
-			var calcul += TtotalHT.value *1;
+			var calcul =+ TtotalHT.value *1;
 			toto.querySelector(".totalPanier").value = calcul;
 		}
 	})
