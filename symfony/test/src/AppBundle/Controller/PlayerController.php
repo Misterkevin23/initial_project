b4cedcf16aa179436077371ca9eab25669d2f4e9
Mister kevin <pascal.d.kevin@gmail.com>
2017-06-19 17:42:26 +0200

    symphony

--- symfony/test/src/AppBundle/Controller/PlayerController.php
+++ symfony/test/src/AppBundle/Controller/PlayerController.php
@@ -33,7 +33,7 @@ class PlayerController extends Controller
 
         $players = $repository -> findAll();
 
-        return $this->render('player/index.html.twig', array(
+        return $this->render('player/index.html.twig', array( //la methode render fournie les donnée a la view
             'titre'     =>$titre,
             'message'   =>'Symfony semble formidable',
             'joueur1'   =>$joueur1,
@@ -43,15 +43,16 @@ class PlayerController extends Controller
     }
     
     /**
-     * @Route("/player/add", name="addplayer")
+     * @Route("/test/player/add", name="testaddplayer")
      */
 
-    public function addAction(Request $request)
+    public function testAddAction(Request $request)
     {
         $player = new Player();
-        $player->setNom("Totti");
-        $player->setPrenom("Francesco");
-        $player->setAge(40);
+        $player->setNom("Diego Armando");
+        $player->setPrenom("Maradonna");
+        $player->setAge(54);
+        $player->setNumeroMaillot(10);
 
         //récupération de l'Entity Manager
         //objet permettant in fine d'intéragir avec la base
@@ -67,5 +68,67 @@ class PlayerController extends Controller
         //On DOIT retourner une reponse HTTP au client
         return new Response('joueur ajouté avec succès');
     }
+
+    /**
+     * @Route("/player/add", name="addplayer")
+     */
+
+    public function addAction (Request $request)
+    {
+        //déterminer si cette route à été demandée en POST ou en GET
+        if($request->isMethod('POST'))
+        {
+            $player = new Player();
+            $player->setNom($request->get('nom'));
+            $player->setPrenom($request->get('prenom'));
+            $player->setAge($request->get('age'));
+            $player->setNumeroMaillot($request->get('numero_maillot')); 
+            $em = $this->getDoctrine()-> getManager();
+            $em->persist($player);
+            $em->flush();
+            // redirection vers la page d'accueil
+            return $this->redirectToRoute('homepage');
+        }
+        else
+        {
+            return $this->render('/player/forms/add.html.twig');
+        }    
+        
+    }
+
+     /**
+     * @Route("/player/{id}", name="detail_player")
+     */
+
+    public function detailAction($id)     //pour récuperer l'id donnée dans l'url, il suffit
+    //de le mettre en argument dans la fonction
+    {
+
+        //->getDoctrine()   Récupère l'ORM
+        //->getManager()    Outil pour opérations en écriture
+        //->getRepository() Outils pour opération en lecture
+        $repository = $this
+                        ->getDoctrine()
+                        ->getManager()
+                        ->getRepository ('AppBundle:Player');
+
+        // récupération de l'id
+        // $id =$request->query->get('id'); //renvoi NULL
+        // var_dump($id);         
+
+        //trouver le joueur correspondant en base de données
+        $player = $repository->find($id); // find() == findById() cherche toujours dans
+        //la colonne id de la table sql
+        
+        //Afficher les informations via une vue/template (fichier twig)
+        //render() associe la vue(fichier.twig) passé en premier argument
+        //avec le tableau associatif passé en deuxième argument
+        //Les données que le controller fournit à la vue seront
+        //accessible (affichables, itérables, etc) par cette dernière               
+        return $this->render('player/detail.html.twig', array(
+            'player' => $player
+        ));
+    }
+
 
 }
